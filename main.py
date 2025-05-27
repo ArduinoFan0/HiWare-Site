dfb6282a3775750952518c244efffecd40623f3b
ArduinoFan0 <100047815+ArduinoFan0@users.noreply.github.com>
2025-05-27 16:01:41 -0600

    a

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from pyscript import document
-import random
-output_div = document.querySelector("#output")
+from pyscript import document, window
+try:
+    import random
+    output_div = document.querySelector("#output")
 
-enable_js_msg = document.querySelector("#enable-js-message")
-js_only_content = document.querySelector("#js-only-content")
-del enable_js_msg
-del js_only_content.attributes.hidden
+    enable_js_msg = document.querySelector("#enable-js-message")
+    js_only_content = document.querySelector("#js-only-content")
+    enable_js_msg.attributes.hidden = True
+    js_only_content.attributes.hidden = False
 
-output_div.innerText = "The Python script is running."
-js_content = document.body.children
-    if element.className == 'js-only-content':
-        element.attributes.hidden = False
-    elif element.className == 'enable-js-message':
-def generate(event):
-    global output_div
-    input_text = document.querySelector("#text_1")
-    my_text = input_text.value
-    output_div.innerText = f"{my_text} - {random.randint(1, 100)}"
+    output_div.innerText = "The Python script is running."
+
+    def generate(event):
+        global output_div
+        input_text = document.querySelector("#text_1")
+        my_text = input_text.value
+        output_div.innerText = f"{my_text} - {random.randint(1, 100)}"
+except BaseException as e:
+    window.reportError(f"Uncaught exception in line {e.__traceback__.tb_lineno} of {__file__}: {type(e).__name__}: {e}")
+    exit(1)
