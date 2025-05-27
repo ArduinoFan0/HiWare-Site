66e1e1751e348200639f7cc42ddedcce0ea28699
ArduinoFan0 <100047815+ArduinoFan0@users.noreply.github.com>
2025-05-27 14:55:11 -0600

    a

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 output_div = document.querySelector("#output")
 output_div.innerText = "The Python script is running."
 def generate(event):
+    global output_div
     input_text = document.querySelector("#text_1")
     my_text = input_text.value
-    output_div = document.querySelector("#output")
     output_div.innerText = f"{my_text} - {random.randint(1, 100)}"
