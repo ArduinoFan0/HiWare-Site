877054430b21efdfb24f1b338206f669fff45be4
ArduinoFan0 <100047815+ArduinoFan0@users.noreply.github.com>
2025-05-27 15:42:52 -0600

    a

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from pyscript import document
 import random
 output_div = document.querySelector("#output")
 output_div.innerText = "The Python script is running."
+js_content = document.body.children
+for element in js_content:
+    if element.className == 'js-only-content':
+        element.attributes.hidden = False
+    elif element.className == 'enable-js-message':
+        del element
 def generate(event):
     global output_div
     input_text = document.querySelector("#text_1")
