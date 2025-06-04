a8b8cb647465513756248aa14389cb90dd913abd
ArduinoFan0 <100047815+ArduinoFan0@users.noreply.github.com>
2025-06-04 12:39:16 -0600

    Update main.py

--- unmanaged_files/8bf577c9296b57e64b23f0e0165465c8da676510b3e48cd648395888025acba8/scripts/main.py
+++ unmanaged_files/8bf577c9296b57e64b23f0e0165465c8da676510b3e48cd648395888025acba8/scripts/main.py
@@ -536,14 +536,14 @@ try:
             self.colliding_body = False
         def update(self):
             rig = document.getElementById("rig")
-            self.x += self.x_velocity * -1 if self.colliding_body else 1
+            self.x += self.x_velocity * (-1 if self.colliding_body else 1)
             if self.colliding_feet:
                 self.y_velocity = max(self.y_velocity, 0)
             self.y += self.y_velocity / self.target_fps
             self.y += self.colliding_feet * self.collision_velocity
             self.collision_velocity += 0.003
 
-            self.z += self.z_velocity * -1 if self.colliding_body else 1
+            self.z += self.z_velocity * (-1 if self.colliding_body else 1)
             self.rotation += self.r_velocity
 
             self.y_velocity -= self.output_gravity / self.target_fps
